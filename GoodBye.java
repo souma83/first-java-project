d4ac2205dd88cf64683e3bcd2bf9505d477e2033
souma83 <52839164+souma83@users.noreply.github.com>
2019-07-12 22:20:21 +0100

    Add files via upload

---
+++ GoodBye.java
@@ -0,0 +1,9 @@
+
+public class GoodBye {
+public static void main(String[]args){
+	System.out.print("Good bye, ");
+	System.out.println("\ncruel world!");
+	System.out.print("asma");
+	System.out.print(" ayadi");
+}
+}
