138bb59ce4170eee3a63dd6f7c84635381e168e7
souma83 <52839164+souma83@users.noreply.github.com>
2019-07-12 22:21:27 +0100

    Update GoodBye.java

--- GoodBye.java
+++ GoodBye.java
@@ -3,7 +3,5 @@ public class GoodBye {
 public static void main(String[]args){
 	System.out.print("Good bye, ");
 	System.out.println("\ncruel world!");
-	System.out.print("asma");
-	System.out.print(" ayadi");
 }
 }
